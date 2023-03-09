d896795ce350f4b07385d1c81e3bc0ecaf217574
Pavel Kizhlo <pavelkizh@gmail.com>
2023-03-09 16:07:00 +0300

    feat: add startscreen animation

--- plam/components/homepage/Startscreen.tsx
+++ plam/components/homepage/Startscreen.tsx
@@ -3,7 +3,12 @@ import styles from '@/styles/Home.module.scss';
 export default function Startscreen() {
   return (
     <section className={styles.startscreen}>
-      <h1 className={styles.startscreen__title}>{'дизайн интерьеров'.toUpperCase()}</h1>
+      <div className={styles.startscreen__wrapper}>
+        <div>
+          <p className={styles.startscreen__title}>{'дизайн интерьеров'.toUpperCase()}</p>
+          <p className={styles.startscreen__title}>{'дизайн интерьеров'.toUpperCase()}</p>
+        </div>
+      </div>
     </section>
   );
 }
