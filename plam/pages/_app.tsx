04b191ec172b0fbc49facb91f7b18cff43f3e085
Pavel Kizhlo <pavelkizh@gmail.com>
2023-03-10 12:02:57 +0300

    feat: add orange lines

--- plam/pages/_app.tsx
+++ plam/pages/_app.tsx
@@ -1,13 +1,10 @@
 import type { AppProps } from 'next/app';
-
 import { useEffect } from 'react';
-
 import AOS from 'aos';
-
+import 'aos/dist/aos.css';
 import Layout from '../components/Layout';
 
 import '@/styles/globals.scss';
-import 'aos/dist/aos.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
