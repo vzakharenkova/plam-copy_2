91580e8b4ad95fa7823e44ce2af58f7f5baf9162
vzakharenkova <v.s.zakharenkova@gmail.com>
2023-03-15 00:10:50 +0300

    feat: add endpoints

--- backend/src/api/home-about/controllers/home-about.ts
+++
@@ -1,7 +0,0 @@
-/**
- * home-about controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::home-about.home-about');
--- backend/src/api/home-about/services/home-about.ts
+++
@@ -1,7 +0,0 @@
-/**
- * home-about service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::home-about.home-about');
